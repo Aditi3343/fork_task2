f50a7cb95a052dd27286a596ab5b8549a5df41b4
Aditi Pandey <117901957+Aditi3343@users.noreply.github.com>
2023-06-28 12:54:18 +0530

    Update splinderBattery.py

--- forage-lyft-starter-repo/battery/splinderBattery.py
+++ forage-lyft-starter-repo/battery/splinderBattery.py
@@ -4,7 +4,7 @@ from utility import add_years_to_date
 class SpindlerBattery(Battery):
     def __init__(self, current_date, last_service_date):
         self.current_date=current_date
-        self.last_service_date=last_service_date  
+        self.last_service_date=last_service_date(self.last_service_date, 2)
     def needs_service(self):
         date_which_battery_should_b_serviced=add_years_to_date  
         if date_which_battery_should_b_serviced < self.current_date:
